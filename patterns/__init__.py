486c81b45916bcbe2f4e210e2f7232173651cb5f
PierreChal <pierre.chalopin@ensta.fr>
2025-04-28 15:05:44 +0200

    V0 test

--- patterns/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Package contenant les design patterns utilisés dans le système de réservation de véhicules.
-"""
-
-from patterns.composite import ComposantParcVehicules, VehiculeComposant
-from patterns.parc_vehicules import ParcVehicules
-
-__all__ = ['ComposantParcVehicules', 'VehiculeComposant', 'ParcVehicules']
