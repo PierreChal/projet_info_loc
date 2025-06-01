cef69bd79d9487fdc82c044705701df88b5bd0e5
PierreChal <pierre.chalopin@ensta.fr>
2025-06-01 16:28:40 +0200

    toutes les BDD

--- view/main2.py
+++ view/main2.py
@@ -207,7 +207,7 @@ class VehiculesScreen(QDialog):
 
 
 
-# --- Code principal ---
+# --- Code principal ----
 app = QApplication(sys.argv)
 widget = QStackedWidget()
 widget.setFixedSize(1200, 800)
